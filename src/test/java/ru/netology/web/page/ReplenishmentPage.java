e7f7d66b91ba10cce69a14419a26e89974124634
Vladislav Romanov <conter123@list.ru>
2024-03-03 18:19:36 +0300

    Realized Page Object's and added tests replenishment card

---
+++ src/test/java/ru/netology/web/page/ReplenishmentPage.java
@@ -0,0 +1,33 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ReplenishmentPage {
+    private SelenideElement head = $("h1");
+    private SelenideElement amountField = $("[data-test-id='amount'] input");
+    private SelenideElement fromField = $("[data-test-id='from'] input");
+    private SelenideElement replenishmentButton = $("[data-test-id='action-transfer']");
+    private SelenideElement errorMessage = $("[data-test-id='error-notification'] notification__content");
+
+    public ReplenishmentPage() {
+        head.shouldHave(exactText("Пополнение карты"));
+    }
+
+    public DashboardPage makeValidReplenishment(String amountToReplenishment, DataHelper.CardInfo cardInfo) {
+        amountField.setValue(amountToReplenishment);
+        fromField.setValue(cardInfo.getCardNumber());
+        replenishmentButton.click();
+        return new DashboardPage();
+    }
+
+
+    public void findErrorMessage(String expectedText) {
+        errorMessage.shouldBe(visible).shouldHave(exactText(expectedText));
+    }
+}
---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,71 @@
+package ru.netology.web.test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTransferTest {
+    DashboardPage dashboardPage;
+    DataHelper.CardInfo firstCardInfo;
+    DataHelper.CardInfo secondCardInfo;
+    int firstCardBalance;
+    int secondCardBalance;
+
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("password_manager_enabled", false);
+        options.setExperimentalOption("prefs", prefs);
+        Configuration.browserCapabilities = options;
+        open("http://localhost:9999");
+        var authInfo = DataHelper.getAuthInfo();
+        var loginPage = new LoginPage();
+        var verificarionPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        dashboardPage = verificarionPage.validVerify(verificationCode);
+        firstCardInfo = DataHelper.getFirstCardInfo();
+        secondCardInfo = DataHelper.getSecondCardInfo();
+        firstCardBalance = dashboardPage.getCardBalance(firstCardInfo.getId());
+        secondCardBalance = dashboardPage.getCardBalance(secondCardInfo.getId());
+    }
+
+    @Test
+    void shouldTransferMoneyFromFirstToCards() {
+        var amount = DataHelper.generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var replenishmentPage = dashboardPage.selectCardToReplenishment(secondCardInfo.getId());
+        dashboardPage = replenishmentPage.makeValidReplenishment(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo.getId());
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo.getId());
+        assertAll(()->assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard),
+                ()->assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard));
+    }
+
+    @Test
+    void shouldGetErrorMassageIfAmountMoreBalance() {
+        var amount = DataHelper.generateInvalidAmount(secondCardBalance);
+        var replenishmentPage = dashboardPage.selectCardToReplenishment(firstCardInfo.getId());
+        replenishmentPage.makeValidReplenishment(String.valueOf(amount), secondCardInfo);
+        replenishmentPage.findErrorMessage("Ошибка! На балансе недостаточно средств");
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo.getId());
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo.getId());
+        assertAll(()->assertEquals(firstCardBalance, actualBalanceFirstCard),
+                ()->assertEquals(secondCardBalance, actualBalanceSecondCard));
+    }
+}
